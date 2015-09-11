f2a1ba953a06bb06939304c9110045909446c4f7
shiguopu <shiguopu@shiguopu-PC>
2015-09-11 14:08:19 +0800

    111

--- HyperStone/src/com/stone/model/Test.java
+++ HyperStone/src/com/stone/model/Test.java
@@ -3,4 +3,14 @@ package com.stone.model;
 public class Test {
 private int id;
 private String name; 
+public static void main(String[] args) {
+	String str="1,2,3,";
+	System.out.println(str.substring(0,str.lastIndexOf(",")));
+	str=","+str;
+	System.out.println(str.replaceFirst(",", ""));
+	
+	
+}
+
+
 }
