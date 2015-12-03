c88e624807a69596a27a835b59f67966073ed5f1
shiguopu <shiguopu@boss.myshow360.com>
2015-12-03 16:59:34 +0800

    aaa

--- HyperStone/src/com/stone/model/Test.java
+++ HyperStone/src/com/stone/model/Test.java
@@ -4,12 +4,16 @@ public class Test {
 private int id;
 private String name; 
 public static void main(String[] args) {
-	String str="1,2,3,";
+	/*String str="1,2,3,";
 	System.out.println(str.substring(0,str.lastIndexOf(",")));
 	str=","+str;
 	System.out.println(str.replaceFirst(",", ""));
+	*/
+	//aaaa
+	Object o=new Integer("12");
+	System.out.println(Long.parseLong(o+""));
 	
-	
+	System.out.println(9L>0);
 }
 
 
